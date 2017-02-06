49f3143d2269504383eedfc47a543d824762d515
Tushar Patekar <tushar.patekar@infogain.com>
2017-02-06 15:36:51 +0530

    added side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { LoginPage } from '../pages/login/login';
+import { MapWithStorePage } from '../pages/map-with-store/map-with-store';
+import { ListWithStorePage } from '../pages/list-with-store/list-with-store';
 
 
 @Component({
@@ -10,8 +12,12 @@ import { LoginPage } from '../pages/login/login';
 })
 export class MyApp {
   rootPage = LoginPage;
-
+  pages: Array<{ title: string, component: any }>;
   constructor(platform: Platform) {
+    this.pages = [
+      { title: 'Store List', component: MapWithStorePage },
+      { title: 'View on Map', component: ListWithStorePage }
+    ];
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
